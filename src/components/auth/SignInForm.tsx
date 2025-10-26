213dbb3e3ba2277923cd69cd8011a379e37b78e7
Marko-sameh <138727717+Marko-sameh@users.noreply.github.com>
2025-10-26 20:55:15 +0300

    Update SignInForm.tsx

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -105,12 +105,6 @@ export default function SignInForm() {
                       Keep me logged in
                     </span>
                   </div>
-                  <Link
-                    to="/reset-password"
-                    className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
-                  >
-                    Forgot password?
-                  </Link>
                 </div>
 
                 {error && (
